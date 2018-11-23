0dea41453edd73db0934e29aa4257f1a5155ab22
abdo <abdoyasser702@gmail.com>
2018-11-23 15:32:39 +0200

    linking to home page

--- server/game_server/game_server/settings.py
+++ server/game_server/game_server/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'game_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
