709b1034c1939c2c393e8bbe784bbd1dc450ea15
abdo <abdoyasser702@gmail.com>
2018-11-23 17:13:32 +0200

    front end home page

--- server/game_server/game_server/urls.py
+++ server/game_server/game_server/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home),
+    path('index/', views.index),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
